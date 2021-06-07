07724b143c8754f6380a1fa08544071017590e20
S Park <spark23@students.cs.ubc.ca>
2021-06-07 00:40:47 -0700

    starting login form

--- vanhouse/src/App.js
+++ vanhouse/src/App.js
@@ -3,6 +3,22 @@ import PostDetail from "./components/PostDetail";
 import "./App.css";
 
 function App() {
+    const testUser = {
+        email: "test@test.com",
+        password: "test123"
+    }
+
+    const [user, setUser] = useState({name: "", email: ""});
+    const [error, setError] = useState("");
+
+    const Login = details => {
+        console.log(details);
+    }
+
+    const Logout = () => {
+        console.log("Logout");
+    }
+
   return (
     <Router>
       <div className="App">
