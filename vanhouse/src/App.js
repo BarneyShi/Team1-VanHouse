570c45941a1af40086d99d829dc565e7c08248f0
Yuxin Tian <43061724+tyxxxx@users.noreply.github.com>
2021-06-10 16:11:00 -0700

    fix userlist

--- vanhouse/src/App.js
+++ vanhouse/src/App.js
@@ -1,7 +1,8 @@
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import PostDetail from "./components/PostDetail";
 import Header from "./components/Header";
-
+import {Container,Row,Col} from 'react-bootstrap';
+import UserList from './components/UserList';
 import "./App.css";
 import PostCollection from "./components/PostCollection";
 
@@ -11,9 +12,16 @@ function App() {
         <Router>
             <div className="App">
                 <Header/>
+                <Container fluid>
+                    <Row>
+                        <Col lg={3} md={3}><UserList /></Col>
+                        <Col>               
                 <Route exact path="/">
                     <PostCollection/>
                 </Route>
+                </Col>
+                    </Row>
+                </Container>
                 <Route path="/post/:id">
                     <PostDetail/>
                 </Route>
---
+++ vanhouse/src/components/UserList.js
@@ -0,0 +1,104 @@
+import {Card,Button,Container,Row,Col} from 'react-bootstrap';
+import {useState} from 'react';
+import user1 from '../assets/img1.jpg';
+import user2 from '../assets/img2.jpg';
+import user3 from '../assets/img3.jpg';
+
+
+function UserList(){
+    const [uname,setName] = useState(["user1","user2","user3"]);
+    const [userimg,setImg] = useState([user1,user2,user3]);
+    var cardList = uname.map((item,index)=>{
+            const style = {
+                width:'35px',
+                borderRadius:'100%',
+                display: 'flex',
+                alignItems: 'center'
+            }
+            const ButtonStyle = {
+                display: 'flex',
+                alignItems: 'center'
+
+            }
+            return(
+                <Row style={{display: 'flex', alignItems: 'center'}}> 
+                   <li><i className="..."></i>&nbsp;</li> 
+                   <Col xs={3} md={3}><img style={style} src={userimg[index]} /></Col> 
+              
+                   <Col xs={3} md={3}> <text style={{display: 'flex', alignItems: 'center',fontSize: '20px'}}> {uname[index]} </text> </Col>
+                
+      
+                   
+
+                   
+                   
+    
+                </Row>
+            )
+    })
+    const [list,setList] = useState(cardList);
+    const handleDelete = (i) =>{
+        var tn = uname;
+        var ti = userimg;
+        tn.splice(i,1);
+        ti.splice(i,1);
+        
+        setName(tn);
+        setImg(ti);
+        //console.log("user:",uname);
+        cardList = uname.map((item,index)=>{
+            const style = {
+                width:'35px',
+                borderRadius:'100%',
+                display: 'flex',
+                alignItems: 'center'
+            }
+
+            const buttonStyle = {
+                display: 'flex',
+                alignItems: 'center'
+
+            }
+            return(
+                <Row style={{display: 'flex', alignItems: 'center'}}>
+                   <Col xs={3} md={3}><img style={style} src={userimg[index]} /></Col> 
+                   
+                   <Col xs={3} md={3}> <text style={{display: 'flex', alignItems: 'center'}}> {uname[index]} </text> </Col>
+                  
+                   <Col xs={3} md={3}><Button variant="outline-primary" size="sm" style={buttonStyle} onClick={()=>handleDelete(index)}>-</Button></Col>
+
+                   
+     
+
+                             
+                </Row>
+                
+                    
+                    
+
+            )
+        })
+        setList(cardList);
+    }
+
+    const boxStyle = {
+
+        boxShadow:'0 8px 20px 0 rgba(0,0,0,0.1), 0 6px 20px 0 rgba(0,0,0,0.1)',
+        borderRadius:'3px',
+        paddingBottom:'10px',
+        height:'90vh'
+    }
+
+    return(
+        <div style={boxStyle}>
+            <h2>User List</h2>
+            <Container fluid>
+                {list}
+            </Container>
+            
+        </div>
+    )
+}
+
+export default UserList;
+
