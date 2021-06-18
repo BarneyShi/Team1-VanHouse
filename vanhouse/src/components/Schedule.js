72fb10b997d9aaecab9a68e24af56cc8862ca77c
BarneyShi <barneyshek@gmail.com>
2021-06-18 01:13:08 -0700

    Set up DatePicker

--- vanhouse/src/components/Schedule.js
+++ vanhouse/src/components/Schedule.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
+import PropTypes from "prop-types";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 export default function Schedule({ show, onHide }) {
-  // CITATION: https://react-bootstrap.github.io/components/modal/
+  // CITATION: Modal https://react-bootstrap.github.io/components/modal/
+  // CITATION: DatePicker https://www.npmjs.com/package/react-datepicker
+
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>
@@ -8,6 +16,14 @@ export default function Schedule({ show, onHide }) {
       </Modal.Header>
       <Modal.Body>
         <p>Tell tenants when you're avaiable for a home tour!</p>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => {
+            console.log(date);
+            setStartDate(date);
+          }}
+          dateFormat="MMM dd yyyy"
+        />
       </Modal.Body>
       <Modal.Footer>
         <Button>Submit</Button>
@@ -15,3 +31,8 @@ export default function Schedule({ show, onHide }) {
     </Modal>
   );
 }
+
+Schedule.prototype = {
+  show: PropTypes.bool.isRequired,
+  onHide: PropTypes.func.isRequired,
+};
