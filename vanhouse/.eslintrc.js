b7f8a915af0b05c4c3a48f48b1f8d1aca1129977
BarneyShi <barneyshek@gmail.com>
2021-07-21 02:10:11 -0700

    Update rules

--- vanhouse/.eslintrc.js
+++ vanhouse/.eslintrc.js
@@ -30,5 +30,6 @@ module.exports = {
     "react/require-default-props": "off",
     "jsx-a11y/click-events-have-key-events": "off",
     "jsx-a11y/no-noninteractive-element-interactions": "off",
+    "react/no-unescaped-entities": "off",
   },
 };
