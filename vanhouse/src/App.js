325e73d7294527474d68f234afd84ae3e90c5d7a
Yuxin Tian <43061724+tyxxxx@users.noreply.github.com>
2021-07-29 19:13:59 -0700

    More cleanup and remove localhost

--- vanhouse/src/App.js
+++ vanhouse/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Admin from "./components/Admin/Admin";
@@ -6,13 +6,8 @@ import NotAuthorized from './components/Admin/NotAuthorized';
 import PostDetail from "./components/PostDetail/PostDetail";
 import Header from "./components/Header";
 import UserList from "./components/UserList";
-import Price from './components/Price';
-import Location from './components/Location';
-import Category from './components/Category';
 import "./App.css";
 import PostCollection from "./components/PostCollection";
-import ResetPassword from "./components/ResetPassword";
-import AccountDetails from "./components/AccountDetails";
 
 function App() {
   const [user, setUser] = useState();
@@ -25,10 +20,10 @@ function App() {
   useEffect( async ()=> {
     try {
       const response = await fetch(
-        "/login-router/account",
-        {
-          credentials: "include",
-        }
+          "/login-router/account",
+          {
+            credentials: "include",
+          }
       );
       if (!response.ok) {
         throw new Error("Not logged in");
@@ -37,77 +32,46 @@ function App() {
       setUser(data);
     } catch (err) {
       setUser();
-      console.log("Error while checking auth:", err.message);
+      console.log("Errow while checking auth:", err.message);
     }
   },[])
 
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/">
-          <Container fluid>
-            <Row id="AppMainRow">
-              <Col lg={3} md={3}>
-                {(filterIdx === 0 && (
-                  <UserList
-                    setReset={setReset}
-                    setQuery={setFilterURL}
-                    setUserId={setUserId}
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/">
+            <Container fluid>
+              <Row id="AppMainRow">
+                <Col lg={3} md={3}>
+                  <UserList setReset={setReset}
+                            setQuery={setFilterURL}
+                            setUserId={setUserId}
                   />
-                )) ||
-                  (filterIdx === 1 && (
-                    <Category
-                      setReset={setReset}
-                      setFilterIdx={setFilterIdx}
-                      setQuery={setFilterURL}
-                    />
-                  )) ||
-                  (filterIdx === 2 && (
-                    <Price
-                      setReset={setReset}
-                      setFilterIdx={setFilterIdx}
-                      setQuery={setFilterURL}
-                    />
-                  )) ||
-                  (filterIdx === 3 && (
-                    <Location
-                      setReset={setReset}
-                      setFilterIdx={setFilterIdx}
-                      setQuery={setFilterURL}
-                    />
-                  ))}
-              </Col>
-              <Col>
-                <PostCollection
-                  filterURL={filterURL}
-                  userId={userId}
-                  setSearchFilter={(i) => {
-                    setFilterIdx(i);
-                    setReset(true);
-                  }}
-                  appPosts={posts}
-                  setAppPosts={setPosts}
-                  setQuery={setFilterURL}
-                />
-              </Col>
-            </Row>
-          </Container>
-        </Route>
-        <Route path="/post/:id">
-          <PostDetail />
-        </Route>
-        <Route path="/admin">
-          {user?.admin ? <Admin />: <NotAuthorized />}
-        </Route>
-        <Route exact path="/resetPassword/:token">
-          <ResetPassword />
-        </Route>
-        <Route exact path="/account">
-          <AccountDetails />
-        </Route>
-      </div>
-    </Router>
+                </Col>
+                <Col>
+                  <PostCollection filterURL={filterURL}
+                                  userId={userId}
+                                  setSearchFilter={(i) => {
+                                    setFilterIdx(i);
+                                    setReset(true);
+                                  }}
+                                  appPosts={posts}
+                                  setAppPosts={setPosts}
+                                  setQuery={setFilterURL}
+                  />
+                </Col>
+              </Row>
+            </Container>
+          </Route>
+          <Route path="/post/:id">
+            <PostDetail />
+          </Route>
+          <Route path="/admin">
+            {user?.admin ? <Admin />: <NotAuthorized />}
+          </Route>
+        </div>
+      </Router>
   );
 }
 
