73d3f9e568d9700102997b71aae32e1b62c57099
S Park <spark23@students.cs.ubc.ca>
2021-07-30 00:02:07 -0700

    trying something else in app.js

--- backend/app.js
+++ backend/app.js
@@ -58,9 +58,25 @@ app.use(function (err, req, res, next) {
 
 
 // for deploying
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'vanhouse', 'build', 'index.html'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'vanhouse', 'build', 'index.html'));
+// });
+
+let protected = ['favicon.ico']
+
+app.get("*", (req, res) => {
+
+  let path = req.params['0'].substring(1)
+
+  if (protected.includes(path)) {
+    // Return the actual file
+    res.sendFile(`${__dirname}/build/${path}`);
+  } else {
+    // Otherwise, redirect to /build/index.html
+    res.sendFile(path.join(__dirname, 'vanhouse', 'build', 'index.html'));
+  }
 });
+
 
 // let secret = require('crypto').randomBytes(64).toString('hex');
 // console.log(secret);
